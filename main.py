7ede452d0fc622e37d548fb61ef5cb717b888b2c
joeht90 <joeht90@icloud.com>
2025-06-09 19:14:32 -0700

    set up parser args for --verbose flag

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import argparse
 from google import genai
 from dotenv import load_dotenv
 
@@ -6,8 +8,23 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(
+
+def main(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("user_input")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+    response = client.models.generate_content(
         model='gemini-2.0-flash-001',
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    )
-print(f"{response.text}\nPrompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens:")
+        contents=argv
+        )
+
+    print(f"{response.text}")
+    if args.verbose:
+          print(f"User prompt: {args.user_input}\n" +
+          f"Prompt tokens: {response.usage_metadata.prompt_token_count}\n" +
+          "Response tokens: "
+          )
+
+
+main(sys.argv[1])
