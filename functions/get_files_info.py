381f24e6f1895fed599f7708d1f427bd761a34a9
joeht90 <joeht90@icloud.com>
2025-06-11 08:59:21 -0700

    get_files_info.py completed

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,10 +1,26 @@
-from pathlib import Path
+import os
+
 
 def get_files_info(working_directory, directory=None):
-    if directory not in working_directory:
+    abs_working_dir = os.path.abspath(working_directory)
+    target_dir = abs_working_dir
+    if directory:
+        target_dir = os.path.abspath(os.path.join(working_directory, directory))
+    if not target_dir.startswith(abs_working_dir):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    if not directory.is_dir():
+    if not os.path.isdir(target_dir):
         return f'Error: "{directory}" is not a directory'
-
-
-
+    try:
+        files_info = []
+        for filename in os.listdir(target_dir):
+            filepath = os.path.join(target_dir, filename)
+            file_size = 0
+            is_dir = os.path.isdir(filepath)
+            if is_dir == False:
+                file_size = os.path.getsize(filepath)
+            files_info.append(
+                f"- {filename}: file_size={file_size} bytes, is_dir={is_dir}"
+            )
+        return "\n".join(files_info)
+    except Exception as e:
+        return f"Error: {e}"
