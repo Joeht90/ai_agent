e14a16e3dc552707fdcb52c2933eff8123f32ca9
joeht90 <joeht90@icloud.com>
2025-06-11 18:29:17 -0700

    completed

--- tests.py
+++ tests.py
@@ -1,9 +1,37 @@
-from main import main
+from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 
 def test():
-    main("what files are in the root?", verbose=False)
-    main("what files are in the pkg directory?", verbose=False)
+    result = get_files_info("calculator", ".")
+    print("Result for current directory:")
+    print(result)
+    print("")
+
+    result = get_files_info("calculator", "pkg")
+    print("Result for 'pkg' directory:")
+    print(result)
+
+    result = get_files_info("calculator", "/bin")
+    print("Result for '/bin' directory:")
+    print(result)
+
+    result = get_files_info("calculator", "../")
+    print("Result for '../' directory:")
+    print(result)
+
+    result = get_file_content("calculator", "main.py")
+    print("Result for 'main.py' file:")
+    print(result)
+
+    result = get_file_content("calculator", "pkg/calculator.py")
+    print("Result for 'pkg/calculator.py' file:")
+    print(result)
+
+    result = get_file_content("calculator", "/bin/cat")
+    print("Result for '/bin/cat' file:")
+    print(result)
+
 
 if __name__ == "__main__":
     test()
