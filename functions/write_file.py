08b855da492514a79617808c85d6a844ec5d14f8
Joeht90 <joeht90@gmail.com>
2025-12-14 20:47:11 -0800

    completed tests for write_file and added all but make new file and write to new file

--- functions/write_file.py
+++ functions/write_file.py
@@ -1,16 +1,19 @@
 import os
 
-def get_file_content(working_directory, file_path=".", content):
+def write_file(working_directory, file_path, content):
     file_path = os.path.join(working_directory, file_path)
     abs_working = os.path.abspath(working_directory)
     target_path = os.path.abspath(os.path.join(working_directory, file_path))
     if not target_path.startswith(abs_working):
         return (
-            f'Error: Cannot read "{file_path}" as it ' 
+            f'Error: Cannot write "{file_path}" as it ' 
             'is outside the permitted working directory'
         )
     if not os.path.isfile(file_path):
         return f'Error: File not found or is not a regular file "{file_path}"'
-
-get_file_content("calculator", "lorem.txt", "wait this isn't lorem ipsum")
+    if not os.path.exists(target_path):
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    with open(file_path, "w") as f:
+        f.write(content)
+    return(f'Successfully wrote to "{file_path}" ({len(content)} characters written)')
 
