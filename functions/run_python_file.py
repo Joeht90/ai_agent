2a844e0e15f92dd2148c3284d534c4b69b043cce
Joeht90 <joeht90@gmail.com>
2025-12-17 13:56:39 -0800

    run_python_file main functionality there just a few errors to work through

---
+++ functions/run_python_file.py
@@ -0,0 +1,25 @@
+import os, subprocess
+
+def run_python_file(working_directory, file_path, args=[]):
+    try:
+        file_path = os.path.join(working_directory, file_path)
+        abs_working = os.path.abspath(working_directory)
+        target_path = os.path.abspath(os.path.join(working_directory, file_path))
+        if not target_path.startswith(abs_working):
+            return(
+            f'Error: Cannot execute "{file_path}" as it '
+            'is outside the permitted working directory'
+            )
+        #if not os.path.exists(target_path):
+            #return(f'Error: File "{file_path}" not found.')
+        if not file_path.endswith(".py"):
+            return(f'Error: "{file_path}" is not a Python file.')
+        result = subprocess.run(file_path, timeout=30, capture_output=True, text=True)
+        if result.stdout != 0:
+            return f"Process exited with code {result.stdout}"
+        if result.stdout == Null and result.stderr == Null:
+            return "No output produce"
+        return f"Process completed. STDOUT: {result.stdout} STDERR: {result.stderr}"
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
+
--- functions/write_file.py
+++ functions/write_file.py
@@ -1,18 +1,22 @@
-import os
+import os 
 
 def write_file(working_directory, file_path, content):
-    file_path = os.path.join(working_directory, file_path)
-    abs_working = os.path.abspath(working_directory)
-    target_path = os.path.abspath(os.path.join(working_directory, file_path))
-    if not target_path.startswith(abs_working):
-        return (
-            f'Error: Cannot write "{file_path}" as it ' 
-            'is outside the permitted working directory'
-        )
+    try:
+        file_path = os.path.join(working_directory, file_path)
+        abs_working = os.path.abspath(working_directory)
+        target_path = os.path.abspath(os.path.join(working_directory, file_path))
+        if not target_path.startswith(abs_working):
+            return (
+                f'Error: Cannot write "{file_path}" as it ' 
+                'is outside the permitted working directory'
+            )
 
-    if not os.path.exists(target_path):
-        os.makedirs(os.path.dirname(file_path), exist_ok=True)
-    with open(file_path, "w") as f:
-        f.write(content)
-    return(f'Successfully wrote to "{file_path}" ({len(content)} characters written)')
+        if not os.path.exists(target_path):
+            os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "w") as f:
+            f.write(content)
+        return(f'Successfully wrote to "{file_path}" '
+            f'({len(content)} characters written)')
+    except Exception as e:
+        f"Error: writing to file: {e}"
 
