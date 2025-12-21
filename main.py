d9f16f56bf7072e334dee223dc4d51ea51108680
Joeht90 <joeht90@gmail.com>
2025-12-20 19:17:43 -0800

    doing my best

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os, argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt as system_prompt
 
 # Create gemini client and pass api_key
 load_dotenv()
@@ -22,7 +23,8 @@ messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)]
 def main():
     response = client.models.generate_content(
         model='gemini-2.5-flash', 
-        contents=messages
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),
     )
     if args.verbose == True:
         print(
